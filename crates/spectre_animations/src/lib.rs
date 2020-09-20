aa0793b9eb01d2320b0f652c738646aaa39bd583
Will Hart <hart.wl@gmail.com>
2020-09-20 16:24:38 +1000

    Implement initial animation system

--- crates/spectre_animations/src/lib.rs
+++ crates/spectre_animations/src/lib.rs
@@ -1,1 +1,97 @@
+use bevy::prelude::*;
 
+pub mod prelude {
+    pub use crate::*;
+}
+
+pub struct AnimationPlugin;
+
+impl Plugin for AnimationPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_system(animate_sprites.system());
+    }
+}
+
+pub type AnimationFrameRange = (usize, usize);
+
+pub struct AnimationState {
+    pub animations: Vec<AnimationFrameRange>,
+    pub current_animation: usize,
+    pub current_idx: usize,
+    pub is_playing: bool,
+}
+
+impl Default for AnimationState {
+    fn default() -> Self {
+        AnimationState {
+            animations: Vec::default(),
+            current_animation: 0,
+            current_idx: 0,
+            is_playing: false,
+        }
+    }
+}
+
+impl AnimationState {
+    // sets the animation on an animation state and moves to the first frame
+    pub fn set_animation(&mut self, animation_idx: usize) -> bool {
+        if animation_idx >= self.animations.len() {
+            println!(
+                "Unknown animation {}, max idx {}",
+                animation_idx,
+                self.animations.len()
+            );
+            return false;
+        }
+
+        self.current_animation = animation_idx;
+        self.current_idx = self.animations[animation_idx].0;
+        return true;
+    }
+
+    // increments the animation frame
+    pub fn incr(&mut self) {
+        if self.current_idx == self.animations[self.current_animation].1 {
+            self.current_idx = self.animations[self.current_animation].0;
+        } else {
+            self.current_idx += 1;
+        }
+    }
+
+    pub fn get_frame_index(&self) -> u32 {
+        if !self.is_playing {
+            return 0;
+        }
+
+        return (self.animations[self.current_animation].0 + self.current_idx) as u32;
+    }
+}
+
+fn animate_sprites(mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &mut AnimationState)>) {
+    for (timer, mut sprite, mut state) in &mut query.iter() {
+        if timer.finished {
+            state.incr();
+            sprite.index = state.get_frame_index();
+        }
+    }
+}
+
+pub fn spawn_animated_spritesheet(
+    mut commands: Commands,
+    texture_atlas_handle: Handle<TextureAtlas>,
+    animation_frames: Vec<(usize, usize)>,
+) {
+    commands
+        .spawn(SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(1.0),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(0.1, true))
+        .with(AnimationState {
+            animations: animation_frames,
+            current_animation: 0,
+            current_idx: 0,
+            is_playing: false,
+        });
+}
