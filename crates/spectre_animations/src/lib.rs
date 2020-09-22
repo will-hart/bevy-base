e018ec27d29c994f594d1d2aed5d5626c733d533
Will Hart <hart.wl@gmail.com>
2020-09-22 14:15:34 +1000

    Add animated spritesheet definitions and loading (#4)
    * Add skeleton lib for spritesheet animations
    
    * Implement initial animation system
    
    * Bump local bevy dependencies
    
    * Spawn playing animation
    
    * Use loaded spritesheet
    
    * Update example to use new loader
    
    * Use id to get animated sprite texture
    
    * Full sprite scene RON
    
    Not sure that this will actually work, just saving for now

---
+++ crates/spectre_animations/src/lib.rs
@@ -0,0 +1,99 @@
+use bevy::prelude::*;
+
+pub mod prelude {
+    pub use crate::*;
+}
+
+pub struct AnimationPlugin;
+
+impl Plugin for AnimationPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_system(animate_sprites.system());
+    }
+}
+
+pub type AnimationFrameRange = (usize, usize);
+
+pub struct AnimationState {
+    pub animations: Vec<AnimationFrameRange>,
+    pub current_animation: usize,
+    pub current_idx: usize,
+    pub is_playing: bool,
+}
+
+impl Default for AnimationState {
+    fn default() -> Self {
+        AnimationState {
+            animations: Vec::default(),
+            current_animation: 0,
+            current_idx: 0,
+            is_playing: false,
+        }
+    }
+}
+
+impl AnimationState {
+    // sets the animation on an animation state and moves to the first frame
+    pub fn set_animation(&mut self, animation_idx: usize) -> bool {
+        if animation_idx >= self.animations.len() {
+            println!(
+                "Unknown animation {}, max idx {}",
+                animation_idx,
+                self.animations.len()
+            );
+            return false;
+        }
+
+        self.current_animation = animation_idx;
+        self.current_idx = self.animations[animation_idx].0;
+        return true;
+    }
+
+    // increments the animation frame
+    pub fn incr(&mut self) {
+        if self.current_idx == self.animations[self.current_animation].1 {
+            self.current_idx = self.animations[self.current_animation].0;
+        } else {
+            self.current_idx += 1;
+        }
+    }
+
+    pub fn get_frame_index(&self) -> u32 {
+        if !self.is_playing {
+            return 0;
+        }
+
+        return (self.animations[self.current_animation].0 + self.current_idx) as u32;
+    }
+}
+
+fn animate_sprites(mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &mut AnimationState)>) {
+    for (timer, mut sprite, mut state) in &mut query.iter() {
+        if timer.finished {
+            state.incr();
+            sprite.index = state.get_frame_index();
+        }
+    }
+}
+
+pub fn spawn_animated_spritesheet(
+    mut commands: Commands,
+    texture_atlas_handle: Handle<TextureAtlas>,
+    frame_duration: f32,
+    animation_frames: Vec<(usize, usize)>,
+    location: Vec3,
+) {
+    commands
+        .spawn(SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(1.0).with_translation(location),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(frame_duration, true))
+        .with(AnimationState {
+            animations: animation_frames,
+            current_animation: 1,
+            current_idx: 0,
+            is_playing: true,
+        });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bevy::{prelude::*, render::pass::ClearColor, window::WindowMode};
+use spectre_animations::prelude::{spawn_animated_spritesheet, AnimationPlugin};
 use spectre_combat::prelude::AllegiancePlugin;
 use spectre_core::prelude::{BuffableStatistic, CharacterStats, Health, Mana, Movement, Stats};
-use spectre_loaders::ResourceLoaderPlugin;
+use spectre_loaders::{ResourceLoaderPlugin, TexturesToLoad};
 use spectre_time::{GameSpeedRequest, GameTimePlugin};
+
+const ANIMATED_SPRITESHEED_ID: u128 = 324890576394765893475;
 
 fn main() {
     App::build()
@@ -18,13 +21,19 @@ fn main() {
         .add_resource(ClearColor(Color::rgb(0.005, 0.005, 0.005)))
         .add_default_plugins()
         .add_startup_system(setup.system())
+        .add_system(spawn_player_debug.system())
         .add_plugin(GameTimePlugin)
         .add_plugin(ResourceLoaderPlugin)
         .add_plugin(AllegiancePlugin)
+        .add_plugin(AnimationPlugin)
         .run();
 }
 
 fn setup(mut commands: Commands) {
+    // spawn the camera
+    commands.spawn(Camera2dComponents::default());
+
+    // spawn a "character" with stats
     commands.spawn(CharacterStats {
         stats: Stats {
             strength: BuffableStatistic::new(10.),
@@ -39,5 +48,37 @@ fn setup(mut commands: Commands) {
         },
     });
 
+    // this loaders approach requires at least one tick of the game loop before
+    // assets handles are available, therefore can't directly spawn player sprite here
+    commands.spawn((TexturesToLoad {
+        textures: vec![("assets/walk_sprite_sheet.png", ANIMATED_SPRITESHEED_ID).into()],
+    },));
+
+    // start the game
     commands.spawn((GameSpeedRequest::new(1.0),));
+}
+
+// demonstrates spawning a player using the spawn_animated_spritesheet helper
+fn spawn_player_debug(
+    commands: Commands,
+    input: Res<Input<KeyCode>>,
+    textures: ResMut<Assets<Texture>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    if !input.just_pressed(KeyCode::Space) {
+        return;
+    }
+
+    let handle: Handle<Texture> = Handle::from_u128(ANIMATED_SPRITESHEED_ID);
+    let texture = textures.get(&handle).unwrap();
+    let texture_atlas = TextureAtlas::from_grid(handle, texture.size, 9, 4);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    spawn_animated_spritesheet(
+        commands,
+        texture_atlas_handle,
+        0.05,
+        vec![(0, 8), (9, 17), (18, 26), (27, 35)],
+        Vec3::new(0., 0., 0.),
+    )
 }
